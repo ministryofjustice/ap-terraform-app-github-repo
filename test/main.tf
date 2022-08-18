215208ca879c2a56a971a05e7375bc7035a17ac0
andyrogers1973 <andy.rogers@digital.justice.gov.uk>
2022-08-18 15:25:01 +0100

    feat: proof of concept code

--- test/main.tf
+++ test/main.tf
@@ -1,3 +1,8 @@
 module "example" {
     source = "./.."
+
+    name = "githubrepo"
+    description = "githubrepotest"
+    template_owner = "gitrepo"
+    template_repository = "templaterepo"
 }
--- variables.tf
+++ variables.tf
@@ -1,4 +1,19 @@
-# variable "example" {
-#   description = "This is an example variable"
-#   type        = string
-# }
+variable "name" {
+    description = "Name of the repository"
+    type        = string
+}
+
+variable "description" {
+    description = "Description for the repository"
+    type        = string
+}
+
+variable "template_owner" {
+    description = "Name of the organisation owning the template repository"
+    type = string
+}
+
+variable "template_repository" {
+    description = "Name of the template repository to use"
+    type = string
+}
