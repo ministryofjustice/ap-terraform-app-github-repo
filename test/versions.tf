fc274cf8629e38a747f53150750aab259b9f427f
andyrogers1973 <andy.rogers@digital.justice.gov.uk>
2022-09-01 09:46:44 +0100

    fix: updated versions.tf

--- test/versions.tf
+++ test/versions.tf
@@ -1,10 +1,10 @@
 terraform {
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.71.0"
+    github = {
+      source  = "integrations/github"
+      version = ">= 4.0"
     }
   }
 
-  required_version = ">= 0.14.0"
+  required_version = ">= 0.14"
 }
